8777335a72211c3b3ed03d55f0056d114e6866e3
Devakumaran27 <devakumaran40@gmail.com>
2025-08-15 16:20:18 +0530

    Commit

---
+++ C2TC_CoreJava_Deva/src/Day3/constructor/ConstructorDemo.java
@@ -0,0 +1,60 @@
+package Day3.constructor;
+
+import java.util.Scanner;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String name, city;
+		int id;
+		String phone;
+		System.out.print("Enter Customer Id : ");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		
+		System.out.print("Enter Customer Phone : ");
+		phone=sc.nextLine();
+
+		System.out.print("Enter Customer Name : ");
+		name=sc.nextLine();
+		
+		System.out.print("Enter Customer City : ");
+		city=sc.nextLine();
+
+		
+		System.out.println(" ");
+		
+		System.out.println("default constructor");
+		Customer c1=new Customer(); 
+		c1.setCustomerName(name);
+		c1.setCustomerId(id);
+		c1.setCustomerCity(city);
+		c1.setCustomerPhone(phone);
+	
+		System.out.println(c1);
+		
+		System.out.print("Enter Customer Id : ");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("Enter Customer Name : ");
+		name=sc.nextLine();
+		
+		System.out.print("Enter Customer City : ");
+		city=sc.nextLine();
+		
+		System.out.print("Enter Customer Phone : ");
+		phone=sc.nextLine();
+		
+		System.out.println("parameterized constructor ");
+		
+		Customer c2=new Customer(name, id, city, phone); 
+		System.out.println(c2);
+		
+
+	}
+
+}
---
+++ C2TC_CoreJava_Deva/src/Day3/constructor/Customer.java
@@ -0,0 +1,68 @@
+package Day3.constructor;
+
+public class Customer {
+	 private String customerName;
+	 private int customerId;
+	 private String customerCity;
+	private String customerPhone;
+	 
+//	System.out.println(" Default Constructor");
+	public Customer() {
+		System.out.println("Default Constructor");
+	}
+	
+	//System.out.println("Parameterized Constructor");
+	//use of this keyword
+	public Customer(String customerName, int customerId, String customerCity, String customerPhone) {
+		
+		this(); //calling default constructor
+		
+		System.out.println("Parameterized Constructor");
+		this.customerName = customerName;
+		this.customerId = customerId;
+		this.customerCity = customerCity;
+		this.customerPhone = customerPhone;
+	}
+	
+	//Getter and Setters
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	
+
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
+	
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+	
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerName=" + customerName + ", customerId=" + customerId + ", customerCity="
+				+ customerCity + ", customerPhone=" + customerPhone + "]"; 
+		
+}
+}
